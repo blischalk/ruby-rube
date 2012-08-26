0ccbff8cfba7d964a451ed5423edd548499449fb
Brett Lischalk <brett@brettlischalk.com>
2012-08-26 10:26:24 -0500

    Added validation of argument existence

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@
 class DataValidator
   class << self
     def validate(string)
-      p string
+      raise ArgumentError, 'You must supply an argument.  Please re-run the program supplying an argument.' if string.nil?
     end
   end
 end
