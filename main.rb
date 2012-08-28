25ecc86f95f71ee6430e943a5bedeaf71dc7abf7
Brett Lischalk <brett@brettlischalk.com>
2012-08-28 16:09:16 -0500

    Added option parsing for switching path

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
 #!/usr/env ruby
 
+require 'optparse'
+
 class DataValidator
   class << self
     def validate(string)
@@ -8,10 +10,16 @@ class DataValidator
   end
 end
 
+class Path
+  attr_accessor :path
+  def initialize(path=1)
+    @path = path
+  end
+end
+
 class DataString
   attr_accessor :data_string
   def initialize(input_string)
-    DataValidator.validate(input_string)
     @data_string = input_string
   end
 end
@@ -26,4 +34,27 @@ class OutputWriter
   end
 end
 
-OutputWriter.new(DataString.new(ARGV[0])).print_output
+class ParsedOptions
+  attr_reader :options
+  def initialize
+    @options = {}
+    optparse = OptionParser.new do |opts|
+      opts.banner = "Usage: optparse1.rb [options] file1 file2 ..."
+      @options[:path] = nil
+      opts.on( '-p', '--path PATH', 'Select which path to run') do |path|
+        @options[:path] = path.to_i
+      end
+    end
+    optparse.parse!
+    puts "We are running path #{@options[:path]}" if @options[:path]
+  end
+end
+
+cmd_opts = ParsedOptions.new()
+case cmd_opts.options[:path]
+when 1
+  DataValidator.validate(ARGV[0])
+  OutputWriter.new(DataString.new(ARGV[0])).print_output
+when 2
+  OutputWriter.new(DataString.new('you awesome Rubyist!')).print_output
+end
