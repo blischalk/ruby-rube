4a087c717b32659fc98759d6e1b5c206b74b7f5f
Brett Lischalk <brett@brettlischalk.com>
2012-09-21 20:31:16 -0500

    Further refactoring

--- main.rb
+++ main.rb
@@ -10,28 +10,41 @@ class DataValidator
   end
 end
 
+# The path that the Contestant will follow
 class Path
-  attr_accessor :path
-  def initialize(path=1)
-    @path = path
+  attr_accessor :obstacles
+  def initialize(obstacles)
+    @obstacles = obstacles
   end
 end
 
+# The object that is following the path of the machine
 class Contestant
-  attr_accessor :obstacle
-  attr_accessor :completed_obstacles
+  attr_accessor :obstacle, :completed_obstacles, :path
   attr_reader :message
   def initialize(message)
     @message=message
     @obstacle = @completed_obstacles = 0
   end
+
   def speech
     puts 'Greetings to all'
     puts @message
     puts "I have conquered #{completed_obstacles} obstacle(s) to be here"
   end
+
+  def run(path)
+    @path = path
+    @path.obstacles.each do |k, v|
+      Obstacle.get(self, k.to_s).start
+    end
+    puts 'Contestent runs to to the podeum to bring his message to the world...'
+    podeum = Podeum.new(self)
+    podeum.present
+  end
 end
 
+# Where the Contestant will end up to deliver the message
 class Podeum
   # Interface takes a DataString instance
   def initialize(contestant)
@@ -42,23 +55,38 @@ class Podeum
   end
 end
 
+class Obstacle
+  attr_reader :contestant
+  attr_accessor :get
+  class << self
+    def get(contestant, obstacle)
+      class_name = obstacle.split('_').select {|w| w.capitalize! || w }.join('')
+      Object.const_get(class_name).new(contestant)
+    end
+  end
+end
+
 # The ladder is an array that is the length of the input string.
 # Initially the string is assigned to the first array item.  The message is
 # moved from each position in the array to the next position until the array has
 # no more positions. When the message gets to the top of the ladder it jumps off....
-class Ladder
-  attr_reader :contestant
+class Ladder < Obstacle
+  attr_accessor :ladder
+
   def initialize(contestant)
     @contestant = contestant
-    @message = @contestant.message
-    setup_ladder(@message)
+    p 'Contestent runs to the first obstacle... The Ladder'
+    setup_ladder(@contestant.message)
     @contestant.completed_obstacles += 1
   end
+
   def setup_ladder(message)
-    @message_length = message.length
-    @ladder = Array.new(@message_length)
+    @ladder = Array.new(message.length)
+  end
+
+  def start
     @ladder.each_with_index do |v, i|
-      @ladder[i] = message
+      @ladder[i] = @contestant.message
       @ladder[i - 1] = nil unless i == 0
       p "Climed on to step #{i +1} of the ladder"
     end
@@ -82,6 +110,8 @@ class ParsedOptions
   end
 end
 
+# Program Class
+# Welcome to the machine...
 class Rube
   def run
     cmd_opts = ParsedOptions.new()
@@ -89,16 +119,18 @@ class Rube
     when 1
       # For this path we need to have an argument supplied on the command line
       DataValidator.validate(ARGV[0])
+      # Acquire a contestant to run the path
       contestant = Contestant.new(ARGV[0])
-      puts 'Contestent runs to the first obstacle... The Ladder'
-      ladder = Ladder.new(contestant)
-      puts 'Contestent runs to to the podeum to bring his message to the world...'
-      podeum = Podeum.new(contestant)
-      podeum.present
+      # Get the path the contestent will follow complete with obstacles
+      path = Path.new({ladder: 0})
+      # Set the contestant running the path
+      contestant.run(path)
+      
     when 2
-      OutputWriter.new('you awesome Rubyist!').print_simple
+      puts 'You awesome Rubyist!'
     end
   end
 end
 
+# Start the Machine
 Rube.new.run
